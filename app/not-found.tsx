0848c85fc497b7fcc4ade90b8455822999438ea7
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-18 22:39:42 +0200

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+// app/not-found.tsx
+
+export const metadata = {
+    title: "404 Not Found - Repo Database Creator!",
+    description: "This Page does not exist Anymore!",
+};
+
+export default function NotFound() {
+    return (
+      <div><head><meta httpEquiv="refresh" content="10;url=https://github.com/ShadowDara/repo-database-creator"></meta></head></div>
+    );
+}
